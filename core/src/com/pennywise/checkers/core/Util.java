63043ec1dfe3858f9c42331f34184f632b949241
Joshua Achoka <jorh.choxxy@gmail.com>
2015-11-03 22:31:18 +0300

    initial collision implementation

--- core/src/com/pennywise/checkers/core/Util.java
+++ core/src/com/pennywise/checkers/core/Util.java
@@ -6,12 +6,17 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 
 /**
  * Created by Joshua.Nabongo on 9/18/2015.
  */
 public class Util {
+
+    private static final String logTag = Util.class.getSimpleName();
 
     public static SpriteDrawable loadTexture(String name) {
         final Texture t = new Texture(Gdx.files.internal(name));
@@ -43,6 +48,74 @@ public class Util {
         font.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 
         return font;
+    }
+
+    /**
+     * Get the rectangle of an actor from its current position and size
+     * */
+    public static Rectangle getRectangleOfActor(Actor actor) {
+        return new Rectangle(actor.getX(), actor.getY(), actor.getWidth(),
+                actor.getHeight());
+    }
+
+    /**
+     * Check collision from actor's rectangles
+     * */
+    public static boolean isActorsCollide(Actor actor1, Actor actor2) {
+        if (Intersector.overlaps(getRectangleOfActor(actor1),
+                getRectangleOfActor(actor2))) {
+            logCollision1(actor1, actor2);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Very precise point detection in a box, think as virtual box in the actual
+     * box with padding as precision amount
+     * */
+    public static boolean isTouchPointCollide(float touchX, float touchY,
+                                              float posX, float posY, float width, float height,
+                                              float precisionAmount) {
+        if (touchX > (posX + precisionAmount)
+                && touchX < (posX + width - precisionAmount)
+                && touchY > (posY + precisionAmount)
+                && touchY < (posY + height - precisionAmount)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Very precise point detection in an actor, think as virtual box in the
+     * actual box with margin as precision amount
+     * */
+    public static boolean isTouchPointCollide(float touchX, float touchY,
+                                              Actor actor, float precisionAmount) {
+        if (touchX > (actor.getX() + precisionAmount)
+                && touchX < (actor.getX() + actor.getWidth() - precisionAmount)
+                && touchY > (actor.getY() + precisionAmount)
+                && touchY < (actor.getY() + actor.getHeight() - precisionAmount)) {
+            logCollision2(actor);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static void logCollision1(Actor a1, Actor a2) {
+        Gdx.app.log(logTag,
+                "Collision detected: Actor (Name: " + a1.getName()
+                        + ") and Actor (Name: " + a2.getName() + ")");
+    }
+
+    private static void logCollision2(Actor a1) {
+        Gdx.app.log(
+                logTag,
+                "Collision detected on touch point: Actor (Name: "
+                        + a1.getName() + ")");
     }
 
 
