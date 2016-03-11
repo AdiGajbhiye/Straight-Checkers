9f25651a0e0aa0a6c7e0cdbd18b4d6a67ad854b4
Joshua Achoka <jorh.choxxy@gmail.com>
2016-03-11 17:23:11 +0300

    commit

--- core/src/com/pennywise/checkers/screens/GameScreen.java
+++ core/src/com/pennywise/checkers/screens/GameScreen.java
@@ -118,7 +118,7 @@ public class GameScreen extends AbstractScreen implements InputProcessor, Multip
     private float[] boardPosition = null;
     private boolean ready = false;
     private float xx, yy;
-    private int playerTurn = Checker.BLACKNORMAL;
+    private int playerTurn = Checker.WHITENORMAL;
     private Label nameLabel;
     private int round = 0;
     private boolean acknowledged = false;
